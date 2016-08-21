067ad3716470f663963798ee9f1e1fcd22b11e4b
jhumbertoh <josehumberto17@gmail.com>
2016-08-21 02:43:11 -0500

    original

--- web-formulario-capcha/src/pe/com/prueba/capcha/web/app/formulario/util/Constantes.java
+++
@@ -1,9 +0,0 @@
-package pe.com.visanet.spr.web.app.formularioafiliacion.util;
-
-public class Constantes {
-
-    public static final String CODIGO_USUARIO_HOMEBANKING = "homebanking";
-    public static final String DOC_EXPORTAR_PDF = "pdfManager";
-    public static final int ID_CONDICION_AFILIACION = 1;
-    public static final int ID_CONDICION_DESAFILIACION = 2;
-}
