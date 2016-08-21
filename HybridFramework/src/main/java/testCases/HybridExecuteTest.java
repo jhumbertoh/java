ca99d315e166f1838783afcc09da1bffa95fabe1
jhumbertoh <josehumberto17@gmail.com>
2016-08-21 01:24:41 -0500

    Original : HybridFramework

---
+++ HybridFramework/src/main/java/testCases/HybridExecuteTest.java
@@ -0,0 +1,78 @@
+package testCases;
+
+import java.io.IOException;
+import java.util.Properties;
+
+import operation.ReadObject;
+import operation.UIOperation;
+
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import excelExportAndFileIO.ReadGuru99ExcelFile;
+
+public class HybridExecuteTest {
+	WebDriver webdriver = null;
+	
+    @Test(dataProvider="hybridData")
+	public void testLogin(String testcaseName,String keyword,String objectName,String objectType,String value) throws Exception {
+		// TODO Auto-generated method stub
+      
+    	
+    	if(testcaseName!=null&&testcaseName.length()!=0){ //D:\Drivers\DriverIE32
+    	
+    		//System.setProperty("webdriver.ie.driver", "D://Drivers//DriverIE32//IEDriverServer.exe");
+
+    		System.setProperty("webdriver.chrome.driver", "D://Drivers//DriverChrome//chromedriver.exe");
+    		
+    		//DesiredCapabilities cap = DesiredCapabilities.internetExplorer();
+            //cap.setCapability(InternetExplorerDriver.INTRODUCE_FLAKINESS_BY_IGNORING_SECURITY_DOMAINS, true);
+    		
+    		//webdriver  = new InternetExplorerDriver(cap);
+    		
+    		webdriver = new ChromeDriver ();
+    		
+    		//webdriver=new FirefoxDriver();
+    	}
+        ReadObject object = new ReadObject();
+        Properties allObjects =  object.getObjectRepository();
+        UIOperation operation = new UIOperation(webdriver);
+      	//Call perform function to perform operation on UI
+    			operation.perform(allObjects, keyword, objectName,
+    				objectType, value);
+    	    
+	}
+
+    
+    @DataProvider(name="hybridData")
+	public Object[][] getDataFromDataprovider() throws IOException{
+    	Object[][] object = null; 
+    	ReadGuru99ExcelFile file = new ReadGuru99ExcelFile();
+        
+         //Read keyword sheet
+         Sheet guru99Sheet = file.readExcel(System.getProperty("user.dir")+"\\","TestCase.xlsx" , "KeywordFramework");
+       //Find number of rows in excel file
+     	int rowCount = guru99Sheet.getLastRowNum()-guru99Sheet.getFirstRowNum();
+     	object = new Object[rowCount][5];
+     	for (int i = 0; i < rowCount; i++) {
+    		//Loop over all the rows
+    		Row row = guru99Sheet.getRow(i+1);
+    		//Create a loop to print cell values in a row
+    		for (int j = 0; j < row.getLastCellNum(); j++) {
+    			//Print excel data in console
+    			object[i][j] = row.getCell(j).toString();
+    		}
+         
+    	}
+     	System.out.println("");
+     	  return object;	 
+	}
+}
