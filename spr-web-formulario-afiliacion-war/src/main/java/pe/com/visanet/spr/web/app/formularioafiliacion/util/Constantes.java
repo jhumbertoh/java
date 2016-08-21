d028208b81e0d73534ce107db93b0d2857e656a7
jhumbertoh <josehumberto17@gmail.com>
2016-08-21 01:50:07 -0500

    Original

---
+++ spr-web-formulario-afiliacion-war/src/main/java/pe/com/visanet/spr/web/app/formularioafiliacion/util/Constantes.java
@@ -0,0 +1,10 @@
+package pe.com.visanet.spr.web.app.formularioafiliacion.util;
+
+public class Constantes {
+
+    public static final String DOC_EXPORTAR_PDF = "pdfManager";
+    public static final int ID_CONDICION_AFILIACION = 1;
+    public static final int ID_CONDICION_DESAFILIACION = 2;
+}
+
+
